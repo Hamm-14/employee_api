c7e14557eb26bd5a832b1c7b08b3b3c04843ffd0
Hammad <hammadyasub14@gmail.com>
2022-05-07 07:09:22 +0530

    add body parser for encoding/decoding

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const port = process.env.PORT || 8000; //use port in env variable for production
 
 const app = express(); //initializing express
 
+app.use(express.urlencoded({ extended: false })); //using body parser
+
 app.listen(port, function (err) {
   if (err) {
     console.log("Error in starting the server", err);
